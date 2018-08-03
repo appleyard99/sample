71739536c6fdb3dbace152620345372a2d5ff1ee
appleyard99 <apple_mgg@sina.com>
2018-08-03 03:05:34 +0000

    finish users signup

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
@@ -9,5 +10,25 @@ class UsersController extends Controller
     public function create(){
 
         return view('users.create');
+    }
+
+    public  function show(User $user){
+        return view('users.show',compact('user'));
+    }
+
+    public function store(Request $request){
+        $result=$this->validate($request,['name'=>'required|max:5','email'=>'required|email|unique:users|max:255','password'=>'required|confirmed|min:6']);
+        //$request->all(); 获取所有请求字段
+        $user=User::create(['name'=>$request->name,'email'=>$request->email,'password'=>bcrypt($request->password)]);
+        //添加注册成功的会话提醒:
+        session()->flash('success','欢迎,您将开启一段新的旅程~');
+        return redirect()->route('users.show',$user);
+        /**
+         * 注意这里是一个『约定优于配置』的体现，此时 $user 是 User 模型对象的实例。route() 方法会自动获取 Model 的主键，也就是数据表 users 的主键 id，以上代码等同于：
+         * redirect()->route('users.show', [$user->id]);
+         * redirect()->route('users.show', $user);也可以
+         *
+         */
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,14 @@ Route::get('/help','StaticPagesController@help')->name('help');
 Route::get('/about','StaticPagesController@about')->name('about');
 Route::get('signup','UsersController@create')->name('signup');// laravl自动适配/*和 * 都可以
 
+Route::resource('users','UsersController');
+//resource()等同于以下路由集合:
+/*
+Route::get('/users','UsersController@index')->name('users.index');//获取用户列表
+Route::get('/users/create','UsersController@create')->name('users.create');//获取用户创建页面
+Route::get('/users/{user}','UsersController@show')->name('users.show');//获取用户个人信息
+Route::post('/users','UsersController@store')->name('users.store');//添加用户
+Route::get('/users/{user}/edit','UsersController@edit')->name('users.edit');//编辑用户页面信息
+Route::patch('/users/{user}','UsersController@update')->name('users.update');//更新用户信息;
+Route::delete('/users/{user}','UsersController@destroy')->name('users.destroy');//删除用户
+*/
